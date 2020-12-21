e59a4a51627a0ceebda3e33a7daef394f9c22dc2
StevenPss <pulessteven@gmail.com>
2020-12-21 20:43:35 +0200

    feat: added simple pagination to posts

--- app/Http/Controllers/WelcomeController.php
+++ app/Http/Controllers/WelcomeController.php
@@ -12,7 +12,7 @@ class WelcomeController extends Controller
     public function index()
     {
         return view('welcome', [
-            'posts' => Post::all(),
+            'posts' => Post::simplePaginate(1),
             'categories' => Category::all(),
             'tags' => Tag::all()
         ]);
