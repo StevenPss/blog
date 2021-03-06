6611477bcc23e7feea0505286ddb09be0c664741
StevenPss <pulessteven@gmail.com>
2021-03-06 22:39:11 +0200

    refactor: fixed sidebar stats

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,11 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use App\Models\Category;
+use App\Models\Post;
+use App\Models\Tag;
+use App\Models\User;
+use Illuminate\Support\Facades\View;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -23,6 +28,14 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        $users_count = User::all()->count();
+        $posts_count = Post::all()->count();
+        $categories_count = Category::all()->count();
+        $tags_count = Tag::all()->count();
+
+        View::share('users_count', $users_count);
+        View::share('posts_count', $posts_count);
+        View::share('categories_count', $categories_count);
+        View::share('tags_count', $tags_count);
     }
 }
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -91,7 +91,28 @@
                         </div>
                     @endif
                     <div class="row">
-                        @include('stats.index')
+                        <div class="col-md-4">
+                            <div class="list-group">
+                                @if (auth()->user()->isAdmin())
+                                    <a href="{{ route('users.index') }}" class="list-group-item list-group-item-action d-flex justify-content-between align-items-center">Users <span class="badge badge-primary badge-pill">@if (isset($users))
+                                        {{ $users}}
+                                    @endif</span></a>
+                                @endif
+                                <a href="{{ route('posts.index') }}" class="list-group-item list-group-item-action d-flex justify-content-between align-items-center">Posts <span class="badge badge-primary badge-pill">@if (isset($posts_count))
+                                        {{ $posts_count }}
+                                    @endif</span></a>
+                                <a href="{{ route('categories.index') }}" class="list-group-item list-group-item-action d-flex justify-content-between align-items-center">Categories <span class="badge badge-primary badge-pill">@if (isset($categories_count))
+                                    {{ $categories_count }}
+                                @endif</span></a>
+                                <a href="{{ route('tags.index') }}" class="list-group-item list-group-item-action d-flex justify-content-between align-items-center">Tags <span class="badge badge-primary badge-pill">@if (isset($tags_count))
+                                    {{ $tags_count }}
+                                @endif</span></a>
+                            </div>
+                        
+                            <div class="list-group">
+                                <a href="{{ route('trashed-posts.index') }}" class="list-group-item list-group-item-action d-flex justify-content-between align-items-center mt-5">Trashed Posts <span class="badge badge-primary badge-pill">2</span></a>                                
+                            </div>
+                        </div>
                         <div class="col-md-8">
                             @yield('content')
                         </div>
